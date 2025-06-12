c3a2c56b11e070f3c7262eea8159a41a4065ded0
Exe <exelcarrizo@gmail.com>
2025-06-12 17:46:56 -0300

    pipes y guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TasksModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProjectsModule, TasksModule, UsersModule],
+  imports: [ProjectsModule, TasksModule, UsersModule, AuthModule],
   controllers: [HelloController, HelloController],
 })
 export class AppModule {}
--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
+import { AuthGuard } from './guards/auth/auth.guard';
 
-@Controller('hello')
+@Controller('')
 export class HelloController {
 
     @Get()
@@ -9,6 +11,33 @@ export class HelloController {
         console.log(request.url)
         response.status(200).send({ message: 'Hello World'});
     }
+
+    @Get('notfound')
+    @HttpCode(404)
+    notFoundPage(){
+        return { message: 'Page not found' };
+    }
+
+    @Get('ticket/:num')
+    getNumber(@Param('num', ParseIntPipe) num: number) {
+        return num + 4;
+    }
+
+    @Get('active/:status')
+    @UseGuards(AuthGuard)
+    isUserActive(@Param('status', ParseBoolPipe) status: boolean) {
+        console.log(typeof status);
+        return status;
+    }
+
+    @Get('greet')
+    @UseGuards(AuthGuard)
+    greet(@Query(ValidateuserPipe) query: { name: string, age: number }) {
+        console.log(typeof query.age);
+        console.log(typeof query.name);
+        return `Hello ${query.name}, you are ${query.age} years old!`;
+    }
+
 
         
 }
